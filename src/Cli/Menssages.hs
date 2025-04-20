084d8e71d728b46aa1491aaf66e8562a0fede6d3
Leosousa-dev <leonardocasa790@gmail.com>
2025-04-20 20:21:21 -0300

    add cli menssages

--- src/Cli/Menssages.hs
+++ src/Cli/Menssages.hs
@@ -3,4 +3,17 @@ module Cli.Menssages where
 welcomeMessage :: String
 welcomeMessage = "\n\nFrank  seu commitador com zero paciência pra seu código meia-boca\n\n"
 
-     
+
+-- Response messages
+
+
+msgFree, msgToken, msgInvalid :: String
+msgFree = "Frank: Modo pobre ativado. Prepare-se pra sinceridade."
+msgToken    = "Frank: Então tá, manda o token aí, celebridade."
+msgInvalid = "Frank: Isso aí nem opção era. Tenta de novo, Einstein."
+
+
+
+-- Menssages  error
+errTokenInvalid :: String
+errTokenInvalid = "Frank: Esse token aí parece mais um CPF aleatório. Tenta de novo."
