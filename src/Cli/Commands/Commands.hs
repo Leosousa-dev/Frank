f06f8ab3b8e8a26c7783a554c8f1d6a140a857b7
Leosousa-dev <leonardocasa790@gmail.com>
2025-04-27 18:32:51 -0300

    add commands

---
+++ src/Cli/Commands/Commands.hs
@@ -0,0 +1,13 @@
+module Cli.Commands.Commands (
+    runInit,
+    runCommit
+    ) where
+
+
+runInit :: IO()
+runInit = putStrLn $ "\n\ncall runInit\n"
+
+
+
+runCommit :: IO()
+runCommit = putStrLn $ "\n\ncall runCommit\n"
--- src/Cli/Parser.hs
+++ src/Cli/Parser.hs
@@ -2,12 +2,13 @@
 module Cli.Parser (runCli) where  
 
 import Cli.Menssages    
+import Cli.Commands.Commands
 
 
 runCli :: [String] -> IO()
 runCli args = do
     let fullCommand = unwords args
     case fullCommand of
-        "frank init" -> putStrLn welcomeMessage
-        "frank commit" -> putStrLn "\n\nCommitting changes...\n\n"
+        "frank init" -> runInit 
+        "frank commit" -> runCommit
         _                ->  putStrLn  $ "\n\n\n\nUnknown command: " ++ fullCommand ++ "\n\n"
