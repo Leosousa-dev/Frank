540adff8bfd92ce9e3e2bccf120c3d8f57497b82
leonardo <leonardocasa790@gmail.com>
2025-05-16 22:45:52 -0300

    feat: integrate OpenAI API
    - Implement callIA function for GPT-3.5-turbo completions
    - Update lens and text dependencies
    - Add HTTP client and JSON support

--- src/Http/Client.hs
+++ src/Http/Client.hs
@@ -2,3 +2,32 @@
 
 module Http.Client () where
 
+
+import Network.Wreq (responseBody, defaults, header, postWith)
+import Control.Lens ((&), (.~), (^.))
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.ByteString.Char8 as BSC
+import Data.Aeson (encode, object, (.=))
+
+
+
+
+callIA :: String -> String -> IO()
+callIA apiKey message = do
+    let url = "https://api.openai.com/v1/chat/completions"
+    let opts = defaults 
+                & header "Content-Type" .~ ["application/json"]
+                & header "Authorization" .~ [BSC.pack ("Bearer " ++ apiKey)]
+    let requestBody = encode $ object
+          [ "model" .= ("gpt-3.5-turbo" :: String)
+          , "temperature" .= (0.7 :: Double)
+          , "messages" .=
+              [ object
+                  [ "role" .= ("user" :: String)
+                  , "content" .= message
+                  ]
+              ]
+          ]
+
+    res <- postWith opts url requestBody
+    LBS.putStrLn (res ^. responseBody)
